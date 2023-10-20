ab5d06d960a5c3e92dd74e7c0a0ad9f5e82b82bf
Ashley <ashley.precios@student.moringaschool.com>
2023-10-20 13:21:36 +0300

    Revert changes

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function fetchBooks() {
   .then ((jsonData) => renderBooks(jsonData))
   }
 
-// function renderBooks(books) {
-//   const main = document.querySelector('main');
-//   books.forEach(book => {
-//     const h2 = document.createElement('h2');
-//     h2.innerHTML = book.name;
-//     main.appendChild(h2);
-//   });
-// }
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
